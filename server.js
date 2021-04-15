cd47830b013195558de2652c7ec59f6a15046e77
Hung Tang <hungtang@hotmail.com>
2021-04-14 21:29:30 -0700

    testing index serve up via express

---
+++ server.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const { notes } = require('./db/db');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
