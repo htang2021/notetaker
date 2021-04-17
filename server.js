5515b731f3f4f5f0c26bcc8f891e33ab18f701cb
Hung Tang <hungtang@hotmail.com>
2021-04-16 18:25:03 -0700

    added routes for both get and post and tested on insomnia

--- server.js
+++ server.js
@@ -19,6 +19,55 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNotes(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add note to json file and notes array in this function
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    // return finished code to post route for response
+    return note;
+}
+
+function validateNotes(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
